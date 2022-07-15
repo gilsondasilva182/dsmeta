2cc50970f7b897600028c66c4a9990fbb1d71a88
gilsondasilva182 <gilsondasilva182@gmail.com>
2022-07-15 20:21:25 -0300

    API test

---
+++ backend/src/main/java/com/devsuperios/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperios.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperios.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
