c218b6eb1338810377cdd9f4b557310f389a9790
gilsondasilva182 <gilsondasilva182@gmail.com>
2022-07-21 23:19:23 -0300

    correção de erros

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,16 @@
-import logo from '../../assets/img/logo.svg'
+import logo from '../../assets/img/logo.svg';
 
-import './styles.css'
+import './styles.css';
+
 function Header(){
     return(
         <header>
         <div className="dsmeta-logo-container">
-            <img src={logo} alt="DSMeta" />
-            <h1>DSMeta</h1>
+            <img src={logo} alt="oveRLoadMeta" />
+            <h1>oveRLoad</h1>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+              <a href="https://www.instagram.com/gilsondasilva182">@gilsondasilva</a>
             </p>
         </div>
     </header>
